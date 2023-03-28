1fde9093463b856cd14a85c700a8f45137cee904
skyeoh <seng.k.yeoh@gmail.com>
2023-03-28 12:15:33 -0400

    Add section on creation of strings
    Strings can be created using single and double quotes.
    Moreover, single quotes can appear within double quotes and vice-versa.
    However, single quotes can appear within single quotes provided
    they are preceded with an escape character "\".
    The same is true with double quotes.

--- 03-data-types/script.js
+++ 03-data-types/script.js
@@ -1,1 +1,16 @@
 console.log("Script is working!");
+
+const sgl = 'Single quotes';
+const dbl = "Double quotes";
+document.getElementById("sgl").innerHTML = sgl;
+document.getElementById("dbl").innerHTML = dbl;
+
+const sglDbl = 'Would you eat a "fish supper"?';
+const dblSgl = "I'm feeling blue.";
+document.getElementById("sglDbl").innerHTML = sglDbl;
+document.getElementById("dblSgl").innerHTML = dblSgl;
+
+const sglSgl = 'Hello \'world\'!';
+const dblDbl = "Hello \"world\"!";
+document.getElementById("sglSgl").innerHTML = 'Hello \\\'world\\\'!\' = \'' + sglSgl;
+document.getElementById("dblDbl").innerHTML = "Hello \\\"world\\\"!\" = \"" + dblDbl;
