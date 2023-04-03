7c73ac56f48f5f71edd7879af5bf9d13b97a551c
skyeoh <seng.k.yeoh@gmail.com>
2023-04-03 15:39:14 -0400

    Add comparison with null and undefined
    Be careful when null and undefined are involved in comparisons

--- 04-conditionals/script.js
+++ 04-conditionals/script.js
@@ -28,3 +28,12 @@ console.log('' == 0);       // true
 console.log(0 === false);   // false
 console.log('' === false);  // false
 console.log('' === 0);      // false
+
+console.log(null === undefined); // false
+console.log(null == undefined);  // true
+console.log(null > 0);           // false
+console.log(null == 0);          // false
+console.log(null >= 0);          // true
+console.log(undefined > 0);      // false
+console.log(undefined < 0);      // false
+console.log(undefined == 0);     // false
