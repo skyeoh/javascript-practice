5c4bea40161522896c6d5baf3ae28306dfe2bb29
skyeoh <seng.k.yeoh@gmail.com>
2023-04-03 13:09:06 -0400

    Add comparison operators
    Also, a link to script.js is added to index.html.

---
+++ 04-conditionals/script.js
@@ -0,0 +1,6 @@
+console.log(5 > 3);     // true
+console.log(5 < 3);     // false
+console.log(5 >= 3);    // true
+console.log(5 <= 3);    // false
+console.log(5 == 3);    // false
+console.log(5 != 3);    // true
