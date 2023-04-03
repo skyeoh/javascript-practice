95e41f27d7975893dcbbc826397935639c88bd72
skyeoh <seng.k.yeoh@gmail.com>
2023-04-03 13:44:13 -0400

    Add string comparison
    The way JavaScript comparison operators work on strings is explored.
    In addition, the result of a comparison expression can be directly
    assigned to a variable, just like any value.

--- 04-conditionals/script.js
+++ 04-conditionals/script.js
@@ -4,3 +4,15 @@ console.log(5 >= 3);    // true
 console.log(5 <= 3);    // false
 console.log(5 == 3);    // false
 console.log(5 != 3);    // true
+
+let result = 10 < 15;
+console.log(result);    // true
+
+console.log('A' < 'Z');         // true
+console.log('Glow' < 'Glee')    // false
+console.log('Beetle' > 'Beet')  // true
+console.log('A' > 'a')          // false
+let chara = 'a',
+    charA = 'A';
+console.log("unicode of 'a' = " + chara.charCodeAt(0));
+console.log("unicode of 'A' = " + charA.charCodeAt(0));
