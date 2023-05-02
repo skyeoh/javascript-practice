8ce6c8aef563dde113de9955192a4c3b1c2db016
skyeoh <seng.k.yeoh@gmail.com>
2023-05-02 15:51:15 -0400

    Add example generated from function declaration

--- 05-functions/script.js
+++ 05-functions/script.js
@@ -1,1 +1,11 @@
 console.log("script.js loaded successfully.");
+
+/***** Function declaration *****/
+const textBox1 = document.querySelector("input#func-declaration");
+const para1 = document.querySelector("p.func-declaration");
+
+textBox1.addEventListener('keydown', logKey);
+
+function logKey(event) {
+    para1.textContent = `You pressed "${event.key}".`;
+}
