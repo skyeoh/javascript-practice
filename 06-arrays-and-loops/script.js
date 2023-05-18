6587ecc30476d41e287841b7b2edc0b09fac691d
skyeoh <seng.k.yeoh@gmail.com>
2023-05-18 14:47:21 -0400

    Add examples of methods to create arrays
    Examples of how arrays can be created using array literal
    and the keyword 'new' are added.

--- 06-arrays-and-loops/script.js
+++ 06-arrays-and-loops/script.js
@@ -1,1 +1,10 @@
 console.log('script.js is linked properly');
+
+const array = ["apple", "orange", 10, 200n, true, false, null, undefined];
+console.log(array);
+
+const array1 = ["apple", 200, true, array, function() { return 10; }];
+console.log(array1);
+
+const array2 = new Array("apple", 200, true, array, function() { return 10; }, null, undefined);
+console.log(array2);
