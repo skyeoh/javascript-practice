f6e4182e06186db70b94d25ce32e9453fb95b1cf
skyeoh <seng.k.yeoh@gmail.com>
2023-06-22 14:13:03 -0400

    Add better description for each test

--- 07-jest/01-sum/sum.test.js
+++ 07-jest/01-sum/sum.test.js
@@ -1,10 +1,10 @@
 const sum = require('./sum');
 
 describe('sum', () => {
-    test('1 + 2', () => {
+    test('1 + 2 = 3', () => {
         expect(sum(1, 2)).toEqual(3);
     });
-    test('10 + 200', () => {
+    test('10 + 200 = 210', () => {
         expect(sum(10, 200)).toEqual(210);
     });
 });
