ad426e219c1b755b423e8183e7c13d0d2aaca329
skyeoh <seng.k.yeoh@gmail.com>
2023-06-23 13:29:41 -0400

    Implement binary search algorithm

--- 07-jest/02-binary_search/binary_search.js
+++ 07-jest/02-binary_search/binary_search.js
@@ -4,11 +4,23 @@
  * Given a sorted array and a target, binarySearch searches
  * for the target in the array. If found, it returns the index
  * of the target; otherwise, it returns -1.
+ * No duplicate entries are allowed in the array.
  */
 
 const binarySearch = function(array, target) {
-
-
+    let start = 0;
+    let end = array.length - 1;
+    while (start <= end) {
+        let mid = Math.floor((start + end)/2);
+        if (array[mid] === target) {
+            return mid;
+        } else if (array[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return -1;
 }
 
 module.exports = binarySearch;
