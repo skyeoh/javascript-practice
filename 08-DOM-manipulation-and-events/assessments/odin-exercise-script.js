697996625bea93f5166e4af62c56dd05f9274e10
skyeoh <seng.k.yeoh@gmail.com>
2023-07-19 16:09:31 -0400

    Add Part 1 of exercise
    A <p> element with red text that says "Hey I'm red!" is added as a
    child to the container element.

--- 08-DOM-manipulation-and-events/assessments/odin-exercise-script.js
+++ 08-DOM-manipulation-and-events/assessments/odin-exercise-script.js
@@ -7,3 +7,11 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+
+/***** Part 1 *****/
+const para = document.createElement('p');
+para.style.color = 'red';
+para.textContent = 'Hey I\'m red!';
+
+container.appendChild(para);
