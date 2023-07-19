0ccbd3d44012faec8a1f0feb233bdc99b139e119
skyeoh <seng.k.yeoh@gmail.com>
2023-07-19 15:59:47 -0400

    Add exercise from Odin Project section "DOM Manipulation and Events"
    This is Part 0, which sets up the context for the other parts.

---
+++ 08-DOM-manipulation-and-events/assessments/odin-exercise-script.js
@@ -0,0 +1,9 @@
+console.log('Successful link to odin-exercise-script.js');
+
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
