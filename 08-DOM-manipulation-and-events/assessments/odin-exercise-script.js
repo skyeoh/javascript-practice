c06aac46c3a98d738a568ecbedcf4586f1d794a9
skyeoh <seng.k.yeoh@gmail.com>
2023-07-21 14:33:58 -0400

    Add Part 3 of exercise
    A <div> element with black border and pink background with two
    child elements is added as a child to the container element.
    The two child elements are an <h1> element that says "I'm in a div"
    and a <p> element that says "ME TOO!".

--- 08-DOM-manipulation-and-events/assessments/odin-exercise-script.js
+++ 08-DOM-manipulation-and-events/assessments/odin-exercise-script.js
@@ -23,3 +23,18 @@ headerLvl3.style.cssText = 'color: blue;';
 headerLvl3.textContent = "I'm a blue h3!";
 
 container.appendChild(headerLvl3);
+
+
+/***** Part 3 *****/
+const div = document.createElement('div');
+div.setAttribute('style', 'border: solid 1px black; background-color: pink;');
+
+const headerLvl1 = document.createElement('h1');
+headerLvl1.textContent = 'I\'m in a div';
+
+const para2 = document.createElement('p');
+para2.textContent = "ME TOO!";
+
+div.appendChild(para2);
+div.insertBefore(headerLvl1, para2);
+container.appendChild(div);
